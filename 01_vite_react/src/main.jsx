434a7f6c58ed534144ab2cf1b7456f70728cf37b
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-09-03 14:45:24 +0600

    main.jsx changed

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <>
+            <h1>Custom App</h1>
+        </>
+    )
+}
 
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit google'
+// }
+
+const ReactElement = React.createElement(
+    
+)
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    anotherElement 
   
 )
