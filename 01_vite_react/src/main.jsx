6b81ee3cbc37d161344ba2127729786f20472a88
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-09-03 18:04:58 +0600

    completed 4th lecture

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -27,13 +27,16 @@ const anotherElement = (
 const ReactElement = React.createElement(
     'a', 
     {href: 'https://chaicode.com', target: '_blank'},
-    'click me to visit chai code website'
+    'click me to visit chai code website',
+    anotherElement /* this is a evaluated  expression where the  variables get injected */
 )
 /*
 here react's createElement method have a syntax as we can see by line:
 1. tar
 2. attribute
-3. inner text */
+3. inner text 
+4. evaluated expression 
+*/
 createRoot(document.getElementById('root')).render(
   
     ReactElement
