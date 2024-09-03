9c33be2a2920a4c8392f02bf68a0fda5fb972434
YusufKhan2016 <hasanrafsun5@gmail.com>
2024-09-03 18:22:36 +0600

    counter project

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1>Hello world</h1>
+     <h2>Counter value: 5</h2>
+     <button>Add Value</button>
+     <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
